75d35473aba4b135235c7352f90942ed8d2d6268
Enricobia <enrico.bianchi.designer@gmail.com>
2018-12-06 17:00:37 +0100

    Add files via upload

---
+++ js/routes.js
@@ -0,0 +1,112 @@
+routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+{
+    path: '/qrcode/',
+    url: './pages/qrcodepage.html',
+  },
+
+{
+    path: '/chat/',
+    url: './pages/chat.html',
+  },
+
+
+
+  {
+    path: '/product/',
+    url: './pages/Product.html',
+  },
+  {
+    path: '/form/',
+    url: './pages/form.html',
+  },
+  // Left View Pages
+  {
+    path: '/left-page-1/',
+    url: './pages/left-page-1.html',
+  },
+  {
+    path: '/left-page-2/',
+    url: './pages/left-page-2.html',
+  },
+  // Page Loaders & Router
+  {
+    path: '/page-loader-template7/:user/:userId/:posts/:postId/',
+    templateUrl: './pages/page-loader-template7.html',
+  },
+  {
+    path: '/page-loader-component/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-component.html',
+  },
+   {
+    path: '/page-loader-balance/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-balance.html',
+  },
+   {
+    path: '/Ethereum Network Stats/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/Ethereum Network Stats.html',
+  },
+
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = routeTo.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            componentUrl: './pages/request-and-load.html',
+          },
+          {
+            context: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
